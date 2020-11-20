d628d5780fa5805aafa16d8c0c3c8ca362d72434
marscarda <52551131+marscarda@users.noreply.github.com>
2020-11-20 03:30:09 -0300

    Post Page to go

--- src/java/arginine/post/WebBackPost.java
+++ src/java/arginine/post/WebBackPost.java
@@ -1,8 +1,18 @@
 package arginine.post;
 //***************************************************************************
 import arginine.WebBackAlpha;
+import serine.blogging.publication.PostRecord;
 //***************************************************************************
 public class WebBackPost extends WebBackAlpha {
+    
+    //***********************************************************************
+    PostRecord post = null;
+    void setPostRecord (PostRecord post) { this.post = post; }
+    public PostRecord getPostRecord () {
+        if (post == null) return new PostRecord();
+        return post;
+    }
+    //***********************************************************************
     
 }
 //***************************************************************************
--- src/java/arginine/post/WebFrontPost.java
+++ src/java/arginine/post/WebFrontPost.java
@@ -5,7 +5,9 @@ import arginine.WebFrontAlpha;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import methionine.AppException;
 import methionine.auth.Session;
+import serine.blogging.publication.PostRecord;
 //***************************************************************************
 @WebServlet(name = "WebFrontPost", urlPatterns = {WebFrontPost.URLPATTERN}, loadOnStartup=1)
 public class WebFrontPost extends WebFrontAlpha {
@@ -25,12 +27,34 @@ public class WebFrontPost extends WebFrontAlpha {
             this.toAuthPage(flowbeta);
             return;
         }
+        //===================================================================
+        long postid = 0;
+        try { postid = Long.parseLong(this.getURLsParamPart(request)); } catch (Exception e) {}
+        //===================================================================
         WebBackPost back = new WebBackPost();
         back.setRootURL(flowbeta.getRootURL());
         back.setLoggedInUser(flowbeta.getLogedUser());
         back.setLoginToken(session.getLoginToken());
         //-------------------------------------------------------------------
-
+        try {
+            PostRecord post = flowbeta.getAurigaObject().getPubsLambda().getPostRecord(0);
+            back.setPostRecord(post);
+        }
+        catch (AppException e) {
+            if (e.getErrorCode() != AppException.OBJECTNOTFOUND) {
+                back.setErrorMessageCode(WebBackPost.ERRINTERNAL);
+                System.out.println("Failed to get Post.");
+                System.out.println("Unexpected error ");
+                System.out.println(e.getMessage());
+            }
+            //else back.setErrorMessageCode(WebBackPost.);
+        }
+        catch (Exception e) {
+            //----------------------------------------------
+            System.out.println("Failed to get post (Web)");
+            System.out.println(e.getMessage());
+            //----------------------------------------------
+        }
         //-------------------------------------------------------------------
         request.setAttribute(PAGEATTRKEY, back);
         //===================================================================
