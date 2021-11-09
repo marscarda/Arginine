4b71edcf0dae24fc7916902e94706ca19f9e7cda
marscarda <52551131+marscarda@users.noreply.github.com>
2021-11-09 15:39:37 -0300

    Closing periods thread start OK

--- src/java/arginine/finantial/ApiCloseOpenPeriods.java
+++ src/java/arginine/finantial/ApiCloseOpenPeriods.java
@@ -3,11 +3,13 @@ package arginine.finantial;
 import arginine.ApiAlpha;
 import static arginine.ApiAlpha.UNAUTHORIZED;
 import arginine.FlowAlpha;
+import histidine.finance.ExcLedgerBackOfice;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mars.jsonsimple.JsonObject;
 import mars.jsonsimple.JsonPair;
+import methionine.AppException;
 import methionine.auth.Session;
 //***************************************************************************
 @WebServlet(name = "ApiCloseOpenPeriods", urlPatterns = {ApiCloseOpenPeriods.URL}, loadOnStartup=1)
@@ -29,19 +31,24 @@ public class ApiCloseOpenPeriods extends ApiAlpha {
             return;
         }
         //==========================================================
-        
-        
-
-        //==========================================================
-        JsonObject jsonresp = new JsonObject();
-        jsonresp.addPair(new JsonPair(RESULT, RESULTOK));
-        jsonresp.addPair(new JsonPair(RESULTDESCRIPTION, "Periods closing started"));
-        //-------------------------------------------------------
-        this.sendResponse(resp, jsonresp);
-        //-------------------------------------------------------
-
-
-    
+        try {
+            ExcLedgerBackOfice exec = new ExcLedgerBackOfice();
+            exec.setAuriga(flowalpha.getAurigaObject());
+            exec.closePeriods(session.getUserId());
+            //-------------------------------------------------------
+            JsonObject jsonresp = new JsonObject();
+            jsonresp.addPair(new JsonPair(RESULT, RESULTOK));
+            jsonresp.addPair(new JsonPair(RESULTDESCRIPTION, "Periods closing started"));
+            //-------------------------------------------------------
+            this.sendResponse(resp, jsonresp);
+        }
+        catch (AppException e) { this.sendErrorResponse(resp, e.getMessage(), e.getErrorCode()); }        
+        catch (Exception e) {
+            sendServerErrorResponse(resp);
+            System.out.println("Failed to add credit");
+            System.out.println(e.getMessage());
+            //----------------------------------------------
+        }
         //==========================================================
         this.finalizeJob(flowalpha);
         this.destroyFlowAlpha(flowalpha);
